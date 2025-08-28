c320efb8f2a156e1ee494231174d074fb3cdf4a1
yuliiabezkorovaina <yuliya.bezkorovayna@gmail.com>
2025-08-28 09:15:15 +0200

    clean styling add new imports tailwind CSS in index.css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,11 +5,11 @@ function App() {
 
   return (
       <>
-          <h1 className="text-3xl font-bold underline">
+          <h1 className="text-black text-3xl font-bold text-center">
               Hello world!
           </h1>
       </>
-  )
+         )
 }
 
 export default App
