caa543abebfdecf9a81679b895d7c9c1feec82cf
yuliiabezkorovaina <yuliya.bezkorovayna@gmail.com>
2025-08-30 19:48:11 +0200

    Frontend: add Types.ts export type Film

--- frontend/src/components/GetAllFilms.tsx
+++ frontend/src/components/GetAllFilms.tsx
@@ -1,15 +1,8 @@
 import axios from "axios";
 import {useEffect, useState} from "react";
+import type {Film} from "./Types.ts";
 
-type Film = {
-    id: string;
-    title: string;
-    release_date: string;
-    rate: number;
-    casts: string;
-    genre: string;
-    duration: number;
-}
+
 export default function GetAllFilms() {
     const [films, setFilms] = useState<Film[]>([]);
 
@@ -27,15 +20,16 @@ export default function GetAllFilms() {
     }, []);
 
     return (
-        <div className="text-black text-3xl font-bold text-center">
+        <div className="text-3xl font-bold text-center">
         <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 sm:gap-8">
             <h1 className="col-span-full text-2xl font-bold mb-4">Films</h1>
-            {films.map((film) => (
+
+                {films.map((film) => (
                 <div key={film.id} className="film-card
                 border-4 border-gray-100 p-4 rounded
                 shadow hover:shadow-lg
                 transition-shadow">
-                    <h1 className="text-lg font-sans">{film.title}</h1>
+                    <h1>{film.title}</h1>
                     <h6>Release Date: {film.release_date
                         ? film.release_date
                         : "-"}</h6>
---
+++ frontend/src/components/Types.ts
@@ -0,0 +1,9 @@
+export type Film = {
+    id: string;
+    title: string;
+    release_date: string;
+    rate: number;
+    casts: string;
+    genre: string;
+    duration: number;
+}
