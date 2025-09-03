7127cabb6e819c2c80209d2f3d1a8eeaff011b4e
yuliiabezkorovaina <yuliya.bezkorovayna@gmail.com>
2025-09-03 21:12:02 +0200

    Frontend: - new IndexPage.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,13 @@
-import AddFilmForm from "./components/AddFilmForm.tsx";
-import GetAllFilms from "./components/GetAllFilms.tsx";
-import {Route, Routes} from "react-router-dom";
-import {EditComponent} from "./components/EditComponent.tsx";
+
+import IndexPage from "./components/IndexPage.tsx";
 
 
 function App() {
 
     return (
-        <>
-            <div className="p-6">
-                <h1 className="text-2xl font-bold mb-4">Add Film</h1>
-                <AddFilmForm  />
 
-            </div>
+            <IndexPage />
 
-                <Routes>
-                    <Route path="/" element={<GetAllFilms />} />
-                    <Route path="/edit/:id" element={<EditComponent />} />
-                </Routes>
-
-
-
-        </>
     );
 }
 
