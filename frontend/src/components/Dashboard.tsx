6c7f177f87576a33debfa1e30f959c79cbed4185
yuliiabezkorovaina <yuliya.bezkorovayna@gmail.com>
2025-09-05 21:44:13 +0200

    Frontend Refactoring: rename GetAllFilms.tsx -> Dashboard.tsx

--- frontend/src/components/Dashboard.tsx
+++ frontend/src/components/Dashboard.tsx
@@ -4,7 +4,7 @@ import type {Film} from "./Types.ts";
 import FilmCard from "./FilmCard.tsx";
 
 
-export default function GetAllFilms() {
+export default function Dashboard() {
     const [films, setFilms] = useState<Film[]>([]);
 
 
@@ -26,6 +26,8 @@ export default function GetAllFilms() {
     };
 
     return (
+
+
         <div className="text-3xl font-bold text-center">
         <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 sm:gap-8">
             <h1 className="col-span-full text-2xl font-bold mb-4">Films</h1>
