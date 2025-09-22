1aab664f69d2c7b81aa08a6b47acb0c4516ddc6f
yuliiabezkorovaina <yuliya.bezkorovayna@gmail.com>
2025-09-22 11:59:29 +0200

    Frontend bugs fixed: - add new icons Filter and Redo - bugs fixed

--- frontend/src/components/UIElements/SearchFilmViewMode.tsx
+++ frontend/src/components/UIElements/SearchFilmViewMode.tsx
@@ -87,7 +87,6 @@ export default function SearchFilmViewMode() {
                     className="flex items-center gap-2 text-white bg-green-600 hover:bg-green-700 font-medium rounded-lg text-sm px-5 py-2.5"
                 >
                     <SaveIcon />
-                    {saving ? "Saving..." : "Add"}
                 </button>
             </div>
         </div>
